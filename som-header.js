f64f31803371d4b58325778249b45c950b536c88
John Arvid <arvid-lindstrom@live.se>
2024-03-11 10:27:36 +0100

    formatting and removing dead code

--- som-header.js
+++ som-header.js
@@ -1,40 +1,58 @@
 //Controllingarrays
-function controllingArrays(desktopControllers, mobileControllers, hideClass, hideClassOnly ){
+
+
+// $.each is a simple iteration construct to loop
+// over an array, collection or object
+
+
+function controllingArrays( desktopControllers, mobileControllers, hideClass, hideClassOnly ){
 
     var controlIsActive = 0;
     //uppdatera controlIsActive
-    $.each(desktopControllers, function(index, value) {
-        if($("[name='setvalue"+value+"']").hasClass("activeRadio")||$("[name='setvalue"+value+"']").hasClass("activeRadiocustom")){
-            controlIsActive+=1;
-        }
-        else {
+    $.each( desktopControllers, function( index, value ) {
+        if( $( "[name='setvalue"+ value + "']" )
+          .hasClass( "activeRadio" )||
+          $( "[name='setvalue" + value + "']" )
+          .hasClass( "activeRadiocustom" ) ) {
+            controlIsActive += 1;
         }
     });
+
     //visa dölj beroende på controlIsActive  
-    if(controlIsActive>0){
-        $(isMobile.matches ? ".responsiveMatrixCell"+hideClass + "," + hideClassOnly: ".responsiveMatrixWeb"+hideClass+ ", " + hideClassOnly).hide();
-    }
-    else {
-        $(isMobile.matches ? ".responsiveMatrixCell"+hideClass + "," + hideClassOnly: ".responsiveMatrixWeb"+hideClass+ ", " + hideClassOnly).show();
+    if ( controlIsActive > 0 ) {
+        $( isMobile.matches ?
+          ".responsiveMatrixCell" + hideClass + "," + hideClassOnly
+          :".responsiveMatrixWeb" + hideClass + ", " + hideClassOnly )
+          .hide();
+    } else {
+        $( isMobile.matches ?
+          ".responsiveMatrixCell" + hideClass + ", " + hideClassOnly
+          :".responsiveMatrixWeb" + hideClass + ", " + hideClassOnly)
+          .show();
     }
     
     //när formuläret uppdateras
-    $("form").change(function(){
-    controlIsActive = 0;
-        if(isMobile.matches){
-            $.each(mobileControllers, function(index, value) {
-                if($("[name='setvalue"+value+"']").hasClass("activeRadio")||$("[name='setvalue"+value+"']").hasClass("activeRadiocustom")){
-                    $("[name='setvalue"+desktopControllers[index]+"']").addClass("activeRadio");
-                    $("[name='"+desktopControllers[index]+"']").prop("checked", "true");
-                }
-                else {
-                    $("[name='setvalue"+desktopControllers[index]+"']").removeClass(["activeRadio", "activeRadiocustom"]);
-                    $("[name='"+desktopControllers[index]+"']").prop("checked", "false");
+    $( "form" ).change( function() {
+      controlIsActive = 0;
+        if ( isMobile.matches ) {
+            $.each( mobileControllers, function( index, value ) {
+                if( $( "[name='setvalue"+value+"']" )
+                .hasClass( "activeRadio" ) ||
+                $( "[name='setvalue" + value + "']" )
+                .hasClass("activeRadiocustom") ) {
+                    $( "[name='setvalue"+desktopControllers[index]+"']" )
+                    .addClass( "activeRadio" );
+                    $( "[name='"+desktopControllers[index]+"']" )
+                    .prop( "checked", "true" );
+                } else {
+                    $( "[name='setvalue" + desktopControllers[index] + "']" )
+                    .removeClass( ["activeRadio", "activeRadiocustom"] );
+                    $("[name='" + desktopControllers[index] + "']" )
+                    .prop( "checked", "false" );
                 }
             });
-        }
-        else{
-            $.each(desktopControllers, function(index, value) {
+        } else {
+            $.each( desktopControllers, function( index, value ) {
                 if($("[name='setvalue"+value+"']").hasClass("activeRadio")||$("[name='setvalue"+value+"']").hasClass("activeRadiocustom")){
                     $("[name='setvalue"+mobileControllers[index]+"']").addClass("activeRadio");
                     $("[name='"+mobileControllers[index]+"']").prop("checked", "true");
@@ -63,8 +81,8 @@ function controllingArrays(desktopControllers, mobileControllers, hideClass, hid
 
 //2022controlledCheckBox.js
 //Styr att frågor visas/döljs med checkbox
-function controlledCheckbox(qids){
-    checkBox = [], //array med checkboxar som styr
+function controlledCheckbox( qids ) {
+    let checkBox = [], //array med checkboxar som styr
     mobileClass = ".responsiveMatrixCell",
     desktopClass = ".responsiveMatrixWeb",
     controlled = ".controlled, .controlledOpen"; //controlled ges till frågor som ska döljas/visas, controlledOpen ges till öppna frågor som ska döljas/visas
@@ -79,14 +97,19 @@ function controlledCheckbox(qids){
         }
     });
 
-    $("form").change(function(){
+    $( "form" ).change( function() {
         $.each(checkBox, function(index, value) {
-            if($(value).hasClass("activeCheckbox")){
-                $(isMobile.matches ? mobileClass+controlled : desktopClass+controlled).hide();
+            if( $( value ).hasClass( "activeCheckbox" ) ){
+                $( isMobile.matches ?
+                  mobileClass + controlled
+                  :desktopClass + controlled )
+                  .hide();
                 return 
-            }
-            else{
-                $(isMobile.matches ? mobileClass+controlled : desktopClass+controlled).show();
+            } else {
+                $( isMobile.matches ?
+                  mobileClass + controlled
+                  :desktopClass + controlled)
+                  .show();
                 return
             }
         });
@@ -95,25 +118,13 @@ function controlledCheckbox(qids){
 
 //END  2022controlledCheckBox.js
 
-//2022updateResponsiveInputs.js
-// function updateResponsiveInputs(){
-//     let desktopInput = $("input[type='text'].responsiveMatrixWeb"),
-//     mobileInput = $("input[type='text'].responsiveMatrixCell");
-// $("input[type='text'].responsiveInput").on("input", function(){
-//     isMobile.matches ? 
-//     desktopInput.val(mobileInput.val())
-//     :mobileInput.val(desktopInput.val());
-//     });
-// $(".responsiveInput").on("change", function(){
-//     isMobile.matches ? 
-//     desktopInput.val(mobileInput.val())
-//     :mobileInput.val(desktopInput.val());
-//     });
-// }
+
+// Refactored below!
 
 function updateResponsiveInputs(){
   let desktopInput = document.querySelector("input[type='text'].responsiveMatrixWeb");
   let mobileInput = document.querySelector("input[type='text'].responsiveMatrixCell");
+
   function updateCorrespondingInput() {
     isMobile.matches ? 
     desktopInput.val(mobileInput.val())
@@ -132,5 +143,15 @@ function removeZebra() {
   const elements = document.querySelectorAll(".removeZebra .StripedRow");
   for (let element in elements) {
       element.classList.remove('StripedRow');
+  }
+}
+
+// Always show open fields
+function alwaysShowOpenFields(){
+  const elements = document.querySelectorAll("input[type='text']");
+  for (let element in elements) {
+    element.setAttribute('disabled', false);
+    element.classList.add('activeConnection');
+    element.classList.remove('passiveconnection');
   }
 }
