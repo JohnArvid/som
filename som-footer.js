645db8b70257ca5d5b4c55e0e443adba15cd70c9
JohnArvid <arvid-lindstrom@live.se>
2024-05-06 08:44:55 +0200

    move function calls into wrapper som() and add eventlistener if document is loading, otherwise run function immediately

--- som-footer.js
+++ som-footer.js
@@ -1,9 +1,16 @@
 // Containerfunction for all invoked onload
-document.addEventListener('DOMContentLoaded', () => {
+function som() {
+  'use strict';
   indikator.setNumericKeyboard();
   indikator.excludeCellOnlyFromAccordion();
   indikator.updateTextInputs();
   indikator.removeZebra();
   indikator.alwaysShowOpenFields();
   indikator.accordion();
-});
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', som);
+} else {
+  som();
+}
