e6612453f5655cdda1683ffe90136d8ad94ecd31
JohnArvid <arvid-lindstrom@live.se>
2024-06-18 16:50:22 +0200

    ta bort disabled på openfields istället för att sätta den till false

--- som-header.js
+++ som-header.js
@@ -256,7 +256,7 @@ const indikator = {
   alwaysShowOpenFields: function () {
     const elements = document.querySelectorAll("input[type='text']");
     elements.forEach((element) => {
-      element.setAttribute('disabled', false);
+      element.attributes.removeNamedItem('disabled');
       element.classList.add('activeConnection');
       element.classList.remove('passiveConnection');
     });
