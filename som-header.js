4239b2b423ebdf660d9569a5914731252d44d30c
JohnArvid <arvid.lindstrom@indikator.org>
2024-06-20 14:45:35 +0200

    single quotes

--- som-header.js
+++ som-header.js
@@ -272,7 +272,7 @@ const indikator = {
 
   alwaysShowOpenFields: function () {
     const elements = document.querySelectorAll("input[type='text']");
-    
+
     elements.forEach((element) => {
       element.disabled = false;
       element.classList.add('activeConnection');
