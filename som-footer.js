6b72946c4f4b29edc97df4758d8e39bcf20e538f
John Arvid <arvid-lindstrom@live.se>
2024-06-20 11:35:38 +0200

    fördröj som() med 0

--- som-footer.js
+++ som-footer.js
@@ -1,13 +1,15 @@
 // Containerfunction for all invoked onload
 function som() {
-  'use strict';
-  indikator.setNumericKeyboard();
-  indikator.excludeFromAccordion();
-  indikator.updateTextInputs();
-  indikator.removeZebra();
-  indikator.checkBoxFilters();
-  indikator.accordion();
-  indikator.samePageFilters();
+  setTimeout(() => {
+    'use strict';
+    indikator.setNumericKeyboard();
+    indikator.excludeFromAccordion();
+    indikator.updateTextInputs();
+    indikator.removeZebra();
+    indikator.checkBoxFilters();
+    indikator.accordion();
+    indikator.samePageFilters();
+  }, 0);
 }
 
 if (document.readyState === 'loading') {
